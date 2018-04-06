7cbccf9beb803d886fbd33ab11db7b8786795227
daftspaniel <daftspaniel@gmail.com>
2018-04-06 17:32:13 +0100

    Working screen.

--- gfx/gameborder.asm
+++ gfx/gameborder.asm
@@ -1,60 +1,37 @@
 *
 * Draw bushes across the screen.
-* 
+*
         ORG $4E21
         LDX #$600 ; Screen base address 
         LDB #$00 ;
-        
-BOXROW  BSR BOX ;
+TOPROW  BSR BUSH ;
         ADDB #$01 ;
         LEAX 1,X ;
         CMPB #$1F ;
-        BLE BOXROW ;
-
+        BLE TOPROW ;
         LDB #$00 ; Draw columns so reset B reg.
         LDX #$700 ; Start address 
-BOXCOL  BSR BOX ;
+LHSCOL  BSR BUSH ;
         ADDB #$01 ;
         LEAX 256,X ; Add row.
         CMPB #$15 ;
-        BLE BOXCOL ;
-        
-SR      LDB #$00 ; Draw columns so reset B reg.
+        BLE LHSCOL ;
+        LDB #$00 ; Draw columns so reset B reg.
         LDX #$71F ; Start address 
-RHSCOL  BSR RHSBOX ;
+RHSCOL  BSR BUSH ;
         ADDB #$01 ;
         LEAX 256,X ; Add row.
         CMPB #$15 ;
         BLE RHSCOL
-        
         LDB #$00 ; 
         LDX #$1D01 ; Start address 
-LOWROW  BSR BOX3 ;
+LOWROW  BSR UBUSH ;
         ADDB #$01 ;
         LEAX 1,X ;
         CMPB #$1D ;
         BLE LOWROW ;
-
         RTS ; EXIT PROGRAM
-
-*-----------------------------------
-* Routines to draw graphic blocks.
-*-----------------------------------
-
-BOX2    LDA #$FF ;
-        STA ,X  ;
-        LDA #$81 ; 
-        STA 32,X ; 
-        STA 64,X ; 
-        STA 128,X ; 
-        STA 160,X ; 
-        STA 192,X ;
-        STA 96,X ; 
-        LDA #$FF ;
-        STA 224,X ;
-        RTS
-
-BOX     LDA #$00 ;
+BUSH    LDA #$00 ;
         STA ,X  ;
         LDA #$28 ; 
         STA 32,X ;
@@ -69,13 +46,23 @@ BOX     LDA #$00 ;
         STA 192,X ;
         STA 224,X ;
         RTS
-
-BOX3    LDA #$BB ;
-        LDA #$BB ; 
+BOX     LDA #$FF ;
+        STA ,X  ;
+        LDA #$81 ; 
+        STA 32,X ; 
+        STA 64,X ; 
+        STA 128,X ; 
+        STA 160,X ; 
+        STA 192,X ;
+        STA 96,X ; 
+        LDA #$FF ;
+        STA 224,X ;
+        RTS
+UBUSH   LDA #$BB ;
         STA 96,X ; 
         LDA #$00 ;
         STA ,X  ;
-        LDA #$FF ;        
+        LDA #$FF ;
         STA 32,X ;
         STA 64,X ;
         LDA #$2B ;
@@ -85,15 +72,5 @@ BOX3    LDA #$BB ;
         LDA #$28 ;
         STA 192,X ;
         LDA #$00 ;
-        STA 224,X  ;
-        RTS
-
-RHSBOX  LDA #$00 ;
-        STA ,X  ;
-        LDA #$27 ; 
-        STA 32,X ;
-        LDA #$1F ; 
-        STA 64,X ;
-        LDA #$2A ;  
-        STA 96,X ; 
+        STA 224,X ;
         RTS
